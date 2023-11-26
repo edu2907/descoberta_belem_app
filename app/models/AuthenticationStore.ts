ac776fda1b7ccf8b5b77846d7b0513711a83ee57
eduardo <eduardo.carneiro2907@gmail.com>
2023-11-25 23:00:25 -0300

    Adicionar Authentication model

---
+++ app/models/AuthenticationStore.ts
@@ -0,0 +1,28 @@
+import { Instance, SnapshotOut, types } from "mobx-state-tree";
+import { api } from "app/services/api"
+
+export const AuthenticationStoreModel = types
+  .model("AuthenticationStore")
+  .props({
+    authToken: types.maybe(types.string),
+    authEmail: "",
+  })
+  .views((store) => ({}))
+  .actions((store) => ({
+    setAuthToken(value?: string) {
+      store.authToken = value;
+    },
+    distributeAuthToken(value?: string) {
+      const token = value || store.authToken;
+      api.apisauce.setHeader("key", token);
+    },
+    setAndDistributeAuthToken(value?: string) {
+      this.setAuthToken(value);
+      this.distributeAuthToken(value);
+    }
+  }));
+
+export interface AuthenticationStore
+  extends Instance<typeof AuthenticationStoreModel> {}
+export interface AuthenticationStoreSnapshot
+  extends SnapshotOut<typeof AuthenticationStoreModel> {}
